158d2d60e0528281450785e02f2838925741f733
Ian C <ian.clark@hotmail.com>
2024-06-19 20:15:50 -0400

    prisma changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
-import Image from 'next/image';
+import prisma from '@/app/lib/db'
 
-export default function Home() {
+export default async function Home() {
+  const events = await prisma.event.findMany();
+
   return (
     <main className='min-h-screen p-24'>
-      <h1>Welcome to NovaRes!</h1>
-      <h2>Your current retreats: </h2>
+      <h1 className='text-2xl'>Welcome to NovaRes!</h1>
+      <h2 className='text-xl'>Your current retreats: </h2>
       <ol>
-        <li>Nothing here!</li>
+        {events.map((event) => (
+          <li key={event.id}>
+            <h3 className='text-lg'>{event.name}</h3>
+            <p className='text-md'>{event.description}</p>
+          </li>
+        ))}
       </ol>
     </main>
   );
