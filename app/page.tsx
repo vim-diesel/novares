7d6c614bc6334ec98a12c2ee70e1118cf13c32e4
Ian C <ian.clark@hotmail.com>
2024-07-15 14:14:50 -0400

    hero section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-export default async function Home() {
+import Hero from './Hero';
 
+export default async function Home() {
   return (
     <main className='min-h-screen'>
-      <div className='mx-auto max-w-7xl mt-10 sm:px-6 lg:px-8'>
-        <h1 className='text-2xl text-center'>Welcome to NovaRes!</h1>
+      <div className='mx-auto max-w-7xl mt-10'>
+        <Hero />
       </div>
     </main>
   );
