53aedf63160d259b93bb1ef5d75611a10b4f553c
Ian C <ian.clark@hotmail.com>
2024-07-25 15:33:11 -0400

    chore: Remove unused ToastMessage and ErrorToast components

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,11 +1,12 @@
-import CreateEvent from './CreateEventForm';
+import CreateEventForm from './CreateEventForm';
 
 
 export default function page() {
 
   return (
     <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-16'>
-      <CreateEvent />
+      <CreateEventForm />
     </div>
   );
 }
+
